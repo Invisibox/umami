01f3fbff4e143478b064a77fd803189df64d6c83
Francis Cao <franciscao@gmail.com>
2024-03-13 18:37:28 -0700

    add postgres session_Id creation to migration script

--- db/postgresql/migrations/05_session_visitor_rename/migration.sql
+++ db/postgresql/migrations/05_session_visitor_rename/migration.sql
@@ -13,7 +13,7 @@ ALTER INDEX "session_website_id_created_at_language_idx" RENAME TO "visitor_webs
 ALTER INDEX "session_website_id_created_at_country_idx" RENAME TO "visitor_website_id_created_at_country_idx";
 ALTER INDEX "session_website_id_created_at_subdivision1_idx" RENAME TO "visitor_website_id_created_at_subdivision1_idx";
 ALTER INDEX "session_website_id_created_at_city_idx" RENAME TO "visitor_website_id_created_at_city_idx";
-5999
+
 -- RenameIndex visitor_data
 ALTER INDEX "session_data_pkey" RENAME TO "visitor_data_pkey";
 ALTER INDEX "session_data_created_at_idx" RENAME TO "visitor_data_created_at_idx";
@@ -32,7 +32,16 @@ ALTER TABLE "session_data" RENAME COLUMN "session_id" TO "visitor_id";
 ALTER TABLE "session_data" RENAME COLUMN "session_key" TO "visitor_key";
 
 ALTER TABLE "website_event" RENAME COLUMN "session_id" TO "visitor_id";
+
+
+-- SessionId
 ALTER TABLE "website_event" ADD COLUMN "session_id" UUID NULL;
+
+UPDATE "website_event"
+SET session_id = uuid_in(overlay(overlay(md5(CONCAT(session_id::text, to_char(date_trunc('hour', created_at), 'YYYY-MM-DD HH24:00:00'))) placing '4' from 13) placing '8' from 17)::cstring)
+WHERE session_id IS NULL;
+
+ALTER TABLE "website_event" ALTER COLUMN "session_id" SET NOT NULL;
 
 -- RenameTable
 ALTER TABLE "session" RENAME TO "visitor";
